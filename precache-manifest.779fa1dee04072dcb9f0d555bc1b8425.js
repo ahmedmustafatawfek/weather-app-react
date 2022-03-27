74923e8ed15b279a7dba7b297a9019e66d613d72
ahmed mustafa <ahmedmustafatawfek96@gmail.com>
2022-03-27 19:49:46 +0200

    Updates

---
+++ precache-manifest.779fa1dee04072dcb9f0d555bc1b8425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5ddf7e6b3883bd2da4a",
+    "url": "/weather-app-react/static/css/main.401dc207.chunk.css"
+  },
+  {
+    "revision": "c5ddf7e6b3883bd2da4a",
+    "url": "/weather-app-react/static/js/main.c5ddf7e6.chunk.js"
+  },
+  {
+    "revision": "9b20288378d2982b1ea9",
+    "url": "/weather-app-react/static/js/1.9b202883.chunk.js"
+  },
+  {
+    "revision": "a69d6614ab32b008890e",
+    "url": "/weather-app-react/static/js/runtime~main.a69d6614.js"
+  },
+  {
+    "revision": "d59b0a1f00e631b0458e1bea022e7920",
+    "url": "/weather-app-react/static/media/sunset.d59b0a1f.jpg"
+  },
+  {
+    "revision": "f37bfe6a6d652476328bda5392a46da0",
+    "url": "/weather-app-react/index.html"
+  }
+];
